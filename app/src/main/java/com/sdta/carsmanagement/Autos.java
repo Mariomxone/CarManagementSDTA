7bae17012ad95ab63e1d647c951013af1da6aa05
Mario Lopez Mendez <mariolopezmendez1111@gmail.com>
2022-03-13 13:52:02 -0600

    Initial commit

---
+++ app/src/main/java/com/sdta/carsmanagement/Autos.java
@@ -0,0 +1,14 @@
+package com.sdta.carsmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Autos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autos);
+    }
+}
